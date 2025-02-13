d71c56d060b7f42fa57a2926e46c65403c87f4cf
suyeon <stgood@kookmin.ac.kr>
2025-02-13 13:39:14 +0900

    feat/prj-setting-1: Spring Boot 프로젝트 세팅 및 회원 엔터티 정의
    Dependencies
    - Lombok: Getter/Setter, 생성자 등을 자동 생성하여 코드 간결화
    - Spring Web: REST API 및 웹 애플리케이션 개발 지원
    - Spring Boot DevTools: 코드 변경 시 자동 리스타트 및 개발 편의성 제공
    - Spring Data JPA: ORM을 활용한 데이터베이스 접근 및 관리
    - MySQL Driver: MySQL과의 연결을 위한 JDBC 드라이버
    
    회원 엔터티
    - Username : ID 역할
    - password
    - nickname : 별명
    - createdAt 과 modifiedAt 은 각각 회원 생성시기와 회원정보수정 시기를 명시하는 속성값이며 BaseTimeEntity 클래스를 상속하여 사용함
    
    BaseTimeEntity 클래스
    - 해당 클래스를 상속하여 엔터티별 생성시기/수정시기를 손쉽게 쓰기 위함

---
+++ src/main/java/com/suyeon/suyeon/entity/Member.java
@@ -0,0 +1,22 @@
+package com.suyeon.suyeon.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Entity
+@Component
+@Table(name = "member")
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String nickname;
+}
